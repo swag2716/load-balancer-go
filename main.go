3f36ac8b40bc7028a17c39e286f0a2c10a129079
swag2716 <swapnikaagrawal@gmail.com>
2025-01-07 22:15:44 +0530

    Created basic load balancer

--- main.go
+++ main.go
@@ -1,46 +1,100 @@
 package main
 
-import(
+import (
 	"fmt"
+	"net/http"
 	"net/http/httputil"
-	"het/http"
+	"net/url"
+	"os"
 )
 
-type Server interface {
-	Address() string
-	IsAlive() bool
-	Serve(w http.ResponseWrite, r *http.Request)
-}
-
-type simpleServer struct{
-	address string
-	proxy *httputil.ReverseProxy
-
-}
-
-type LoadBalancer struct {
-	port string
-	roundRobbinCount int
-	servers []Server
-}
-
-func newSimpleServer(add string) *simpleServer{
-	serverUrl, err := url.Parse(add) 
-	handleErr(err)
-
-	return &simpleServer{
-		address: add,
-		proxy: httputil.NewSingleHostReverseProxy(serverUrl)
-	}
-}
-
-func handleErr(err error){
+func handleErr(err error) {
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
 		os.Exit(1)
 	}
 }
 
-func main(){
-	
+type Server interface {
+	Address() string
+	IsAlive() bool
+	Serve(rw http.ResponseWriter, r *http.Request)
+}
+
+type SimpleServer struct {
+	Addr  string
+	Proxy httputil.ReverseProxy
+}
+
+func NewSimpleServer(address string) *SimpleServer {
+	serverURL, err := url.Parse(address)
+	handleErr(err)
+	server := &SimpleServer{
+		Addr:  address,
+		Proxy: *httputil.NewSingleHostReverseProxy(serverURL),
+	}
+
+	return server
+}
+
+func InitLoadBalancer(port string, servers []Server) *LoadBalancer {
+	LoadBalancer := &LoadBalancer{
+		Port:             port,
+		RoundRobbinCount: 0,
+		Servers:          servers,
+	}
+
+	return LoadBalancer
+}
+
+func (lb *LoadBalancer) GetNextAvailableServer() Server {
+	servers := lb.Servers
+	server := servers[lb.RoundRobbinCount%len(servers)]
+	for !server.IsAlive() {
+		lb.RoundRobbinCount++
+		server = servers[lb.RoundRobbinCount%len(servers)]
+	}
+	lb.RoundRobbinCount++
+
+	return server
+}
+
+func (lb *LoadBalancer) ServeProxy(rw http.ResponseWriter, r *http.Request) {
+	server := lb.GetNextAvailableServer()
+	fmt.Println("forwarding request to:", server.Address())
+	server.Serve(rw, r)
+
+}
+
+func (s *SimpleServer) IsAlive() bool {
+	return true
+}
+func (s *SimpleServer) Address() string {
+	return s.Addr
+}
+func (s *SimpleServer) Serve(rw http.ResponseWriter, r *http.Request) {
+	s.Proxy.ServeHTTP(rw, r)
+}
+
+type LoadBalancer struct {
+	Port             string
+	RoundRobbinCount int
+	Servers          []Server
+}
+
+func main() {
+
+	servers := []Server{NewSimpleServer("https://www.google.com/"), NewSimpleServer("http://www.duckduckgo.com/"), NewSimpleServer("https://www.instagram.com/")}
+
+	lb := InitLoadBalancer("8000", servers)
+
+	handleRedirect := func(rw http.ResponseWriter, r *http.Request) {
+		lb.ServeProxy(rw, r)
+	}
+
+	http.HandleFunc("/", handleRedirect)
+
+	fmt.Println("Listening on port:", lb.Port)
+
+	http.ListenAndServe(":"+lb.Port, nil)
 }
